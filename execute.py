3bd2b33d4e685ff563b0450fe6a82e4f383c06d1
James <miserochan@gmail.com>
2017-08-04 15:37:16 -0400

    Made imports cleaner.

--- execute.py
+++ execute.py
@@ -5,7 +5,7 @@ filename = sys.argv[1]
 contents = open(filename).read()
 lines = contents.split("\n")
 if(lines[0].startswith("import ")):
-    imports = lines[0].replace("import ","").split(",")
+    imports = lines[0].replace("import ","").split(", ")
     contents = contents.replace(lines[0],"")
     for filename in imports:
         contents = open(filename).read() + contents
